59565b4d325d9a85d728227cafd1262d15889882
kstumpf <kstumpf@smith.edu>
2018-02-28 20:58:29 -0500

    2Sum

---
+++ 2Sum/2Sum.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left, bisect_right
+
+# Kaitlyn Stumpf
+# 02/10/2018
+
+# Algorithms Review
+# 2Sum
+
+def main():
+    f = open("2Sum.txt")
+    lines = f.readlines()
+
+    nums = set()
+    for line in lines:
+        nums.add(int(line))
+
+    arr = sorted(nums)
+
+    print(twoSum(arr))
+
+
+def twoSum(arr):
+    vals = set()
+
+    for num in arr:
+        low = bisect_left(arr, -10000 - num)
+        high = bisect_right(arr, 10000 - num)
+        for pairNum in arr[low:high]:
+            if pairNum != num:
+                vals.add(num + pairNum)
+    return len(vals)
+
+if __name__ == "__main__":
+    main()
