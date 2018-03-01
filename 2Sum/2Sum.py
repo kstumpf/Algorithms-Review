48562c769feabdb040ff9ac11371cf54c85b6221
kstumpf <kstumpf@smith.edu>
2018-02-28 22:05:20 -0500

    Notes

--- 2Sum/2Sum.py
+++ 2Sum/2Sum.py
@@ -6,6 +6,11 @@ from bisect import bisect_left, bisect_right
 # Algorithms Review
 # 2Sum
 
+# Input: Unsorted array of n int. Target sum t.
+# Goal: Determine whether or not there are two # x, y in A with x + y = t
+# Insert el of A into hashtable H.
+# For each x in A, lookup t - x in H.
+
 def main():
     f = open("2Sum.txt")
     lines = f.readlines()
